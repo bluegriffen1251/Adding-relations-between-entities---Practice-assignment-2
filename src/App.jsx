f74e883f295864f1c93cb7775c6c355cf325ca89
Jagathpriyan <bluegriffen1856@gmail.com>
2025-03-25 14:33:48 +0530

    finial

--- src/App.jsx
+++ src/App.jsx
@@ -30,12 +30,33 @@ const initialProducts = [
 ];
 
 function App() {
+  const [products, setProducts] = useState(initialProducts);
+
 
  
 
+  const handleRatingSubmit = (productId, newRating) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((product) =>
+        product.id === productId
+          ? {
+              ...product,
+              avgRating: ((product.avgRating * product.totalRatings) + newRating) / (product.totalRatings + 1),
+              totalRatings: product.totalRatings + 1,
+            }
+          : product
+      )
+    );
+  };
+
   return (
-    <div>
-     {/* code here */}
+
+    <div className="App">
+
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} onRatingSubmit={handleRatingSubmit} />
+      ))}
+
     </div>
   );
 }
